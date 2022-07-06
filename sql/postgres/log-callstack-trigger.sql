872e3de94daab4e5d92acff31042d5aed4c0b129
Aleksandr Vlasenko <a.yu.vlasenko0@gmail.com>
2022-07-06 13:09:10 +0300

    refactor: increase code readability

--- sql/postgres/log-callstack-trigger.sql
+++ sql/postgres/log-callstack-trigger.sql
@@ -12,37 +12,37 @@ begin
             _callstack = PG_EXCEPTION_CONTEXT;
     end;
 
-    if (
-        new.some_column is distinct from old.some_column
-    ) then
-        perform some_log_json_function(json_build_object(
-            'type', 'log_some_table_some_column_update_callstack',
-            'id', new.id,
-            'new.some_column', new.some_column,
-            'old.some_column', old.some_column,
-            'query', (
-                select
-                    json_build_object(
-                        'usename', pg_stat_activity.usename,
-                        'applcation_name', pg_stat_activity.application_name,
-                        'client_addr', pg_stat_activity.client_addr,
-                        'sql', pg_stat_activity.query
-                    )
-                from pg_stat_activity
-                where
-                    pg_stat_activity.pid = pg_backend_pid()
-                limit 1
-            ),
-            'callstack', (
-                select array_agg( line )
-                from unnest(
-                    regexp_split_to_array(_callstack, '[\n\r]+')
-                ) as line
-                where
-                    line ilike '%line % at SQL statement%'
-            )
-        )::text);
+    if ( new.some_column is not distinct from old.some_column ) then
+        return new;
     end if;
+
+    perform some_log_json_function(json_build_object(
+        'type', 'log_some_table_some_column_update_callstack',
+        'id', new.id,
+        'new.some_column', new.some_column,
+        'old.some_column', old.some_column,
+        'query', (
+            select
+                json_build_object(
+                    'usename', pg_stat_activity.usename,
+                    'applcation_name', pg_stat_activity.application_name,
+                    'client_addr', pg_stat_activity.client_addr,
+                    'sql', pg_stat_activity.query
+                )
+            from pg_stat_activity
+            where
+                pg_stat_activity.pid = pg_backend_pid()
+            limit 1
+        ),
+        'callstack', (
+            select array_agg( line )
+            from unnest(
+                regexp_split_to_array(_callstack, '[\n\r]+')
+            ) as line
+            where
+                line ilike '%line % at SQL statement%'
+        )
+    )::text);
 
     return new;
 end
